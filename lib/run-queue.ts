d735c4967a742378643f36cbab779e28e7b716a6
slawa <slawa@server>
2024-10-19 00:28:23 +0200

    feat: build with yaml param feat: move built file into storage

--- lib/run-queue.ts
+++ lib/run-queue.ts
@@ -2,24 +2,70 @@ import { Job } from "bull";
 import { Logger } from "@/lib/splunk";
 import spawn from "nano-spawn";
 import { queue } from "@/lib/queue";
+import { Project } from "@/app/project";
+import yaml from "yaml";
+import * as fs from "node:fs";
+import path from "node:path";
+import invariant from "tiny-invariant";
+import mv from "mv";
+import { promisify } from "node:util";
+
+const mvAsync = promisify(mv);
+
+const flutterProjectRoot = "/home/slawa/dev/flutter_site_container";
 
 (async () => {
-  void queue.process(1, async (job: Job<{ title: string }>) => {
-    Logger.info(`Processing job ${job.id}`);
-    const cwd = "/home/slawa/dev/flutter_site_container";
-    const cmd = "script -c 'flutter build appbundle'";
+  void queue.process(1, async (job: Job<Project>) => {
+    try {
+      Logger.info(`Processing job ${job.id}`);
 
-    Logger.info(cwd, cmd);
-    for await (const line of spawn(cmd, {
-      cwd,
-      shell: true,
-    })) {
-      Logger.info(line);
-      await job.log(line);
+      const steps = [generateConfigYamlFile, runBuild, storeBuildResult];
+
+      for await (let step of steps) {
+        Logger.info(step.name);
+        await step(job);
+        await job.progress(1);
+      }
+
+      Logger.info("processing job", job.id, "done");
+    } catch (e) {
+      Logger.error(e);
+      invariant(e instanceof Error, "e must be an Error");
+      await job.moveToFailed(e);
     }
-    await job.progress(1);
-    Logger.info("processing job", job.id, "done");
   });
 
   Logger.info("ready to process");
 })();
+
+async function generateConfigYamlFile(job: Job<Project>) {
+  const yamlText = yaml.stringify(job.data);
+  console.log(yamlText);
+  fs.writeFileSync(
+    path.join(flutterProjectRoot, "assets/config.yaml"),
+    yamlText,
+  );
+}
+
+async function runBuild(job: Job<Project>) {
+  const cmd = "script -c 'flutter build appbundle'";
+  Logger.info(flutterProjectRoot, cmd);
+  for await (const line of spawn(cmd, {
+    cwd: flutterProjectRoot,
+    shell: true,
+  })) {
+    Logger.info(line);
+    await job.log(line);
+  }
+}
+
+async function storeBuildResult(job: Job<Project>) {
+  const androidFile = path.join(
+    flutterProjectRoot,
+    "build/app/outputs/bundle/release/app-release.aab",
+  );
+  const destination = `/tmp/web2app/project-${job.data.id}/job-${job.id}/app-release.aab`;
+  fs.mkdirSync(path.dirname(destination), { recursive: true });
+  mvAsync(androidFile, destination, { mkdirp: true });
+  Logger.info("moved", androidFile, "to", destination);
+}
--- lib/splunk.ts
+++ lib/splunk.ts
@@ -1,26 +1,37 @@
 import bunyan from "bunyan";
-import splunkBunyan from "splunk-bunyan-logger";
-import consoleStream from "bunyan-console-stream";
+// import BFormat from "bunyan-format";
+import * as bunyanDebugStream from "bunyan-debug-stream";
+// const config = {
+//   token: process.env.SPLUNK_TOKEN,
+//   url: "https://localhost:8088",
+// };
 
-const config = {
-  token: process.env.SPLUNK_TOKEN,
-  url: "https://localhost:8088",
-};
+// const splunkStream = splunkBunyan.createStream(config);
+// splunkStream.on("error", function (err, context) {
+//   // Handle errors here
+//   console.log("Error", err, "Context", context);
+// });
 
-const splunkStream = splunkBunyan.createStream(config);
-splunkStream.on("error", function (err, context) {
-  // Handle errors here
-  console.log("Error", err, "Context", context);
-});
+// let formatOut = new BFormat({ outputMode: "short" });
 
 // Note: splunkStream must be set to an element in the streams array
 export const Logger = bunyan.createLogger({
   name: "web2app",
+  serializers: bunyanDebugStream.serializers,
   streams: [
-    splunkStream,
+    // { type: "raw", stream: formatOut },
+    // splunkStream,
+    // {
+    //   type: "raw",
+    //   stream: consoleStream.createStream(),
+    // },
     {
+      level: "info",
       type: "raw",
-      stream: consoleStream.createStream(),
+      stream: bunyanDebugStream.create({
+        basepath: process.cwd(), // this should be the root folder of your project.
+        forceColor: true,
+      }),
     },
   ],
 });
