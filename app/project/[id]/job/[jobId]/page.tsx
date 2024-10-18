c9be404f6ab97534b4990d0a736626aa171b535d
slawa <slawa@server>
2024-10-18 23:35:15 +0200

    feat: display log output nicer

--- app/project/[id]/job/[jobId]/page.tsx
+++ app/project/[id]/job/[jobId]/page.tsx
@@ -1,20 +1,45 @@
 "use client";
 
 import { useSwrApi } from "@/app/use-swr-api";
+import { Job } from "bull";
+import type { Project } from "@/app/project";
+import { RenderTable } from "@/app/render-table";
+import stripAnsi from "strip-ansi";
+import Link from "next/link";
+
+export interface JobWithLogs extends Job<Project> {
+  logs: {
+    count: number;
+    logs: string[];
+  };
+}
 
 export default function Project({
   params,
 }: {
   params: { id: string; jobId: string };
 }) {
-  const { item } = useSwrApi(
+  const { item } = useSwrApi<JobWithLogs>(
     `/api/project/${params.id}/queue/job/`,
     params.jobId,
   );
+  let { data: job, logs } = item.data ?? {};
+  let logsTable = (logs?.logs?.map((x) => ({
+    logLine: stripAnsi(x),
+  })) ?? []) as { logLine: string }[];
   return (
     <div>
-      <div>ID: {params.jobId}</div>
-      <pre>data: {JSON.stringify(item.data, null, 2)}</pre>
+      <div className="text-2xl my-3">
+        Project Id: <Link href={`/project/${params.id}`}>{params.id}</Link>
+      </div>
+      <div className="text-xl my-3">Job Id: {params.jobId}</div>
+      <pre className="my-3">data: {JSON.stringify(job, null, 2)}</pre>
+      {logs && (
+        <RenderTable
+          columns={[{ key: "logLine", label: "" }]}
+          data={logsTable}
+        />
+      )}
     </div>
   );
 }
