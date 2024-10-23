5b6253d8a6122d37cf8308733d3c5658da4c11e0
slawa <slawa@server>
2024-10-23 02:29:55 +0200

    next 15

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["react-helpers", "spidgorny-react-helpers"],
+  serverExternalPackages: ['bull'],
 };
 
 export default nextConfig;
